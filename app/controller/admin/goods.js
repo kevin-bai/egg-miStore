e14a1589300a87cd4fe9a374614d48a014434a22
Kevin J Bai <897840029@qq.com>
2019-04-03 15:02:48 +0800

    feat (model & controller): 添加商品，添加商品属性

--- app/controller/admin/goods.js
+++ app/controller/admin/goods.js
@@ -3,6 +3,9 @@ const BaseController = require('./base.js');
 
 class GoodsController extends BaseController {
   async index() {
+    let goods = await this.ctx.model.Goods.find({})
+    console.log('goods',goods)
+
     await this.ctx.render('admin/goods/index');
   }
 
@@ -46,10 +49,10 @@ class GoodsController extends BaseController {
     let newGoods = new this.ctx.model.Goods(formFields)
     let result = await newGoods.save();
 
+    // 添加商品相册
     if (result._id && formFields.goods_image_list) {
       let goods_image_list = formFields.goods_image_list;
-
-      if(typeof(goods_image_list === 'strin')){
+      if (typeof (goods_image_list === 'strin')) {
         goods_image_list = new Array(goods_image_list)
       }
 
@@ -62,6 +65,40 @@ class GoodsController extends BaseController {
       }
     }
 
+
+    // 添加商品类型数据
+    if (result._id && formFields.attr_id_list && formFields.attr_value_list) {
+      let attr_id_list = formFields.attr_id_list;
+      let attr_value_list = formFields.attr_value_list;
+
+      if(typeof(attr_id_list) === 'string'){
+        attr_id_list = new Array(attr_id_list)
+        attr_value_list = new Array(attr_value_list)
+      }
+
+      for (let i = 0; i < attr_id_list.length; i++) {
+        let goodsTypeAttributeResult = await this.ctx.model.GoodsTypeAttribute.findOne({'_id': attr_id_list[i]})
+        
+        let newGoodsAttr = new this.ctx.model.GoodsAttr({
+          goods_id: result._id,
+          cate_id:formFields.cate_id,
+          attribute_id: attr_id_list[i],
+          attribute_type: goodsTypeAttributeResult.attr_type,
+          attribute_title: goodsTypeAttributeResult.title,
+          attribute_value: attr_value_list[i],
+        })
+
+        await newGoodsAttr.save()
+
+      }
+
+
+
+    }
+
+
+
+    await this.success('/admin/goods','添加商品成功');
 
 
   }
