dd0d861a7d2a43554da73cd4665a8fae2595c6c6
Kevin J Bai <897840029@qq.com>
2019-04-09 15:28:51 +0800

    feat  (mvc)： 文章分类curd && 文章curd

---
+++ app/controller/admin/article.js
@@ -0,0 +1,180 @@
+const fs = require('fs');
+
+const pump = require('mz-modules/pump');
+
+let BaseController = require('./base.js');
+class ArticleController extends BaseController {
+  async index() {
+    let page = this.ctx.request.query.page || 1;
+    let pageSize = 3;
+
+    //总数量
+    let totalNum = await this.ctx.model.Article.find({}).countDocuments();
+
+
+
+    /*
+              
+       let goodsResult=await this.ctx.model.Goods.find({}).skip((page-1)*pageSize).limit(pageSize);
+       
+    */
+
+
+    //让文章和分类进行关联
+
+    let result = await this.ctx.model.Article.aggregate([
+
+      {
+        $lookup: {
+          from: 'article_cate',
+          localField: 'cate_id',
+          foreignField: '_id',
+          as: 'catelist'
+        }
+      },
+      {
+        $skip: (page - 1) * pageSize
+      },
+      {
+        $limit: pageSize
+      }
+
+    ])
+
+    console.log(result);
+
+
+    await this.ctx.render('admin/article/index', {
+      list: result,
+      totalPages: Math.ceil(totalNum / pageSize),
+      page: page
+    });
+
+  }
+  async add() {
+
+    //获取所有的分类
+    let cateResult = await this.ctx.model.ArticleCate.aggregate([
+
+      {
+        $lookup: {
+          from: 'article_cate',
+          localField: '_id',
+          foreignField: 'pid',
+          as: 'items'
+        }
+      },
+      {
+        $match: {
+          "pid": '0'
+        }
+      }
+
+    ])
+
+
+    await this.ctx.render('admin/article/add', {
+
+      cateList: cateResult
+    });
+
+  }
+
+  async doAdd() {
+
+    let parts = this.ctx.multipart({
+      autoFields: true
+    });
+    let files = {};
+    let stream;
+    while ((stream = await parts()) != null) {
+      if (!stream.filename) {
+        break;
+      }
+      let fieldname = stream.fieldname; //file表单的名字
+
+      //上传图片的目录
+      let dir = await this.service.tools.getUploadFile(stream.filename);
+      let target = dir.uploadDir;
+      let writeStream = fs.createWriteStream(target);
+
+      await pump(stream, writeStream);
+
+      files = Object.assign(files, {
+        [fieldname]: dir.saveDir
+      })
+
+      //生成缩略图
+      this.service.tools.jimpImg(target);
+
+
+    }
+
+    let article = new this.ctx.model.Article(Object.assign(files, parts.field));
+    await article.save();
+
+    await this.success('/admin/article', '增加文章成功');
+
+  }
+
+
+  async edit() {
+
+    let id = this.ctx.request.query.id;
+
+    //当前id对应的数据
+    let result = await this.ctx.model.Article.find({
+      "_id": id
+    });
+
+    //获取所有的分类
+    let cateResult = await this.ctx.model.ArticleCate.aggregate([
+
+      {
+        $lookup: {
+          from: 'article_cate',
+          localField: '_id',
+          foreignField: 'pid',
+          as: 'items'
+        }
+      },
+      {
+        $match: {
+          "pid": '0'
+        }
+      }
+
+    ]);
+
+    await this.ctx.render('admin/article/edit', {
+      cateList: cateResult,
+      list: result[0],
+      prevPage: this.ctx.state.prevPage
+    });
+
+  }
+
+  async doEdit() {
+
+    let uploadResult = await this.service.tool.getUploadFile(this.ctx)
+    //let formFields = Object.assign(uploadResult.files, uploadResult.field)
+
+    let id = uploadResult.field.id;
+
+    let prevPage = uploadResult.field.prevPage;
+
+    let formFields = Object.assign(uploadResult.files, uploadResult.field)
+
+    await this.ctx.model.Article.updateOne({
+      "_id": id
+    }, formFields);
+
+    await this.success(prevPage, '修改数据成功');
+
+
+
+  }
+
+
+}
+module.exports = ArticleController;
