572d2ffc2809e6b9cdd500efb961e411f051aab2
Kevin J Bai <897840029@qq.com>
2019-04-11 14:38:20 +0800

    res (images): 导入静态资源图片

--- app/controller/default/index.js
+++ app/controller/default/index.js
@@ -4,7 +4,33 @@ const Controller = require('egg').Controller;
 
 class IndexController extends Controller {
   async index(){
-    await this.ctx.render('/default/index')
+    let topNav = await this.ctx.model.Nav.find({"position":1})
+
+    let focus = await this.ctx.model.File.find({});
+
+    let goodsCate = await this.ctx.model.GoodsCate.aggregate([
+      {
+        $lookup:{
+          from:'goods_cate',
+          localField:'_id',
+          foreignField:'pid',
+          as:'items'
+        }
+      },
+      {
+        $match:{
+          "pid":'0'
+        }
+      }
+    ])
+    console.log('goods cate',goodsCate)
+
+
+    await this.ctx.render('/default/index',{
+      topNav,
+      focus,
+      goodsCate
+    })
   }
 }
 
